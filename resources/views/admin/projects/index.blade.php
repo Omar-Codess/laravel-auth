9e471af9cb2049e856d34687549d4c75cc4c25cb
Omar-Codess <omartaki8@gmail.com>
2023-01-13 18:46:51 +0100

    aggiunto bottone in index per editare

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -30,6 +30,9 @@
                                     <a class="btn btn-success" href="{{ route('admin.projects.show', $project->slug) }}">
                                         <i class="fa-solid fa-eye"></i>
                                     </a>
+                                    <a class="btn btn-warning" href="{{ route('admin.projects.edit', $project->slug) }}">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
                                 </td>
                             </tr>
                         @endforeach

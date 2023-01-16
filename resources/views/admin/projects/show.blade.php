27547e4399f77ab0fd4648364117e0e48e77a369
Omar-Codess <omartaki8@gmail.com>
2023-01-16 18:56:22 +0100

    stampata immagine a schermo

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,6 +8,9 @@
             </a>
         </div>
         <h2 class="text-center">{{ $project->title }}</h2>
+        <div class="mt-3 text-center">
+            <img class="w-50" src="{{ asset('storage/' . $project->cover_image) }}" alt="">
+        </div>
         <p class="mt-3">{{ $project->description }}</p>
     </div>
 @endsection
